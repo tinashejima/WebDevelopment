b90698065036f6ef8b23025816de2118709274a7
tinashejima <130299508+tinashejima@users.noreply.github.com>
2024-03-18 09:41:26 +0200

    add flies
    Add Project folder

---
+++ Web Design Mini Project/venue.js
@@ -0,0 +1,9 @@
+let disply = document.querySelector("#home");
+let back = document.querySelector(".imagesContainer");
+let prev = document.querySelector(".imgDetails");
+
+disply.addEventListener( type:"click", listener: () => {
+    back.classList.remove("notActive")
+    prev.classList.add("notActive")
+
+})

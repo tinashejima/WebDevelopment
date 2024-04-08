66b77beed81581656f7a8e6d6df0c9a09cdc7954
tinashejima <jimatinashe@gmail.com>
2024-04-08 07:36:04 +0200

    added new files and renamed a folder

--- Web Design Mini Project/venue.js
+++ Web Design Mini Project/venue.js
@@ -1,9 +1,19 @@
-let disply = document.querySelector("#home");
-let back = document.querySelector(".imagesContainer");
-let prev = document.querySelector(".imgDetails");
+//document.addEventListener('DOMContentLoaded', function(){
 
-disply.addEventListener(type:"click", listener () => {
-    back.classList.remove("notActive")
-    prev.classList.add("notActive")
+    //document.querySelector('#aboutProject', function dsplay(){
 
-})
+        //if (document.querySelector('#aboutProject').innerHTML==='none') {
+        //    document.querySelector('#aboutProject').innerHTML='show';
+
+        //} else {
+
+           // document.querySelector('#aboutProject').innerHTML='show!'
+//}
+        
+         // Step 2: Add an event listener to the link
+         document.getElementById('aboutProject').addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent the default link behavior
+        // Step 3: Toggle the visibility of the hidden div
+        var aboutProjectDiv = document.getElementById('aboutProjectDiv');
+        aboutProjectDiv.style.display = (aboutProjectDiv.style.display === 'none') ? 'block' : 'none';
+    });
